d967b5a56c509c839095f6d1a0b633d401efbc64
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-09-16 22:57:51 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcSouls (start, goal) {
+    for (let i=start; i < goal; i++){
+        console.log(i)
+    }
+}
+
+var start_level = document.querySelector('.start_level')
+var goal_level = document.querySelector('.goal_level')
+var output = document.querySelector('.output')
